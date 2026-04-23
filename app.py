188ab3972c6705708722142fc04fe01448e73aac
arjunpawansharma <arjunnomiddlenamesharma@gmail.com>
2026-04-23 01:46:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ a_start, a_end = st.slider("Pages with Answer Key", 1, len(doc), st.session_stat
 q_start = 1
 q_end = a_start - 1
     
-    st.markdown("### 2. Build the Exam")
+   st.markdown("### 2. Build the Exam")
     if st.button("Scan PDF & Build Exam", type="primary"):
         st.warning("⏳ Scanning questions and cross-referencing the answer key... This takes time. Please do not refresh!")
         
